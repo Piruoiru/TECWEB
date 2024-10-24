ff2d63433145bc7b435237d4685b936c6816a4fe
Piruoiru <matteopiron2003@gmail.com>
2024-10-24 18:47:55 +0200

    sistemato cose per iniziare

--- web/header.php
+++ web/header.php
@@ -1,8 +1,29 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>ValeryumLand</title>
+        <title>Mirabilia Park</title>
         <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="keywords" content="parco, divertimento, giostre, weekend, mirabiliapark">
+
         <link rel="stylesheet" href="/style/style.css">
+
     </head>
     <body>
+        <header>
+            <h1>Federazione Italiana Pallavolo</h1>
+            <h2>La pallavolo femminile italiana</h2>
+        </header>
+        <nav>
+            <p>Ti trovi in: <span lang="en">Home</span></p>
+        </nav> 
+        <nav id="menu">
+            <ul>
+                <li><a href="/profilo.php">Profilo</a></li>
+                <li><a href="/register.php">Register</a></li>
+                <li><a href="/login.php">Login</a></li>
+                <li><a href="/logout.php">Logout</a></li>
+            </ul>
+
+            <p>Valerio stai zitto coglione</p>
+        </nav>
