d9c7e6bd2e0799fa5c9337154c52fe2082257d43
Vinny <163838894+VinnYuri@users.noreply.github.com>
2024-12-10 13:12:10 +0100

    Update calendar.php

--- web/calendar.php
+++ web/calendar.php
@@ -2,4 +2,36 @@
     include_once 'parser.php';
     $template = new Parser();
     $template->render('calendar.html');
+    class Calendar
+{
+  public static function create($month)
+  {
+    $data = $month;
+    $giorniSett = ['Lunedì','Martedì','Mercoledì','Giovedì','Venerdì','Sabato','Domenica'];
+    $giorniDelMese = date("t",strtotime($data));
+    $primoGiorno = date("N",strtotime($data));
+    $giorni = array_fill(0, ($primoGiorno - 1), '');
+
+    for ($i = 1; $i <= $giorniDelMese; $i++) {
+      $giorni[] = $i;
+    }
+
+    $calendario = array_chunk($giorni, 7);
+    $nextMese = date("Y-M", strtotime($data . "+ 1 month"));
+    $lastMese = date("Y-M", strtotime($data . "- 1 month"));
+    $mese = date("M", strtotime($data));
+    $annoMese = date("Y", strtotime($data));
+
+    $data = array(
+      'next' => $nextMese,
+      'mese' => $mese,
+      'anno' => $annoMese,
+      'last' => $lastMese,
+      'calendario' => $calendario,
+      'settimane' => $giorniSett,
+    );
+
+    return $data;
+  }
+}
 ?>
