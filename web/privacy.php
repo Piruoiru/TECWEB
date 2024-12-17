b8301a64f07ba3a98f541828171fb8b5555413dd
Piruoiru <matteopiron2003@gmail.com>
2024-12-17 17:08:58 +0100

    created page privacy

--- web/privacy.php
+++ web/privacy.php
@@ -1,5 +1,7 @@
 <?php
     include_once 'parser.php';
+    $context = ['orario'=>"10:00-17:00"];
+
     $template = new Parser();
-    $template->render('privacy.html');
+    $template->render('privacy.html',$context);
 ?>
