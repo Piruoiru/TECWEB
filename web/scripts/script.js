6cf3dd57f1aa0041bb4816e22eec810cdc98eaef
7eo03 <leonardosalviato03@gmail.com>
2024-12-22 20:55:06 +0100

    Fix selezione data

--- web/scripts/script.js
+++ web/scripts/script.js
@@ -1,4 +1,3 @@
-const picked = document.getElementById("picked");
 const month = document.getElementById("month");
 const calendar = document.getElementById("calendar");
 
@@ -7,24 +6,22 @@ let thisMonth = DATE.getMonth();
 let year = DATE.getFullYear();
 
 const MONTHS = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
+  "Gennaio",
+  "Febbraio",
+  "Marzo",
+  "Aprile",
+  "Maggio",
+  "Giugno",
+  "Luglio",
+  "Agosto",
+  "Settembre",
+  "Ottobre",
+  "Novembre",
+  "Dicembre",
 ];
 
-picked.innerHTML = `${DATE.getDate()}/${thisMonth + 1}/${year}`;
-
-const createCalendar = () => {
-  month.innerHTML = `${MONTHS[thisMonth]}, ${year}`;
+function buildCalendar(){
+  month.innerHTML = `${MONTHS[thisMonth]} ${year}`;
 
   const dayOne = (new Date(year, thisMonth).getDay()+6)%7;
   const monthDays = 32 - new Date(year, thisMonth, 32).getDate();
@@ -33,38 +30,37 @@ const createCalendar = () => {
   for (let i = 0; i < 6; i++) {
     let row = document.createElement("tr");
     for (let j = 0; j < 7; j++) {
-      let column = document.createElement("td");
+      let cell = document.createElement("td");
       if (date > monthDays) break;
       else if (i === 0 && j < dayOne) {
-        let columnText = document.createTextNode("");
-        column.appendChild(columnText);
-        row.appendChild(column);
+        let cellText = document.createTextNode("");
+        cell.appendChild(cellText);
+        row.appendChild(cell);
       } else {
         let columnText = document.createTextNode(date);
-        column.appendChild(columnText);
+        cell.appendChild(columnText);
 
         let hoursText = document.createElement("p");
         hoursText.textContent = "9:00 - 18:00";
-        hoursText.style.fontSize = "0.8em";
-        column.appendChild(hoursText);
+        cell.appendChild(hoursText);
 
         if(date === DATE.getDate() && thisMonth === DATE.getMonth() && year === DATE.getFullYear()){
-          column.classList.add("today")
+          cell.classList.add("today")
         }
 
-        column.onclick = () => {
-          picked.innerHTML = `${column.columnText}/${thisMonth + 1}/${year}`; //DA SISTEMARE
-        };
+        let cellDay = `${date}`;
+        let cellMonth = `${thisMonth + 1}`;
+        let cellYear = `${year}`;
 
-        row.appendChild(column);
+
+        cell.onclick = () => dateSelected(cellDay, cellMonth, cellYear);
+
+        row.appendChild(cell);
 
         // Aggiungi tabIndex per navigazione
-        column.setAttribute("tabindex", "0");
-        column.setAttribute("role", "button");
-        column.setAttribute("aria-label", `Giorno ${date}`);
-        column.onclick = () => {
-          picked.innerHTML = `${date}/${thisMonth + 1}/${year}`; //DA SISTEMARE
-        };
+        // column.setAttribute("tabindex", "0");
+        // column.setAttribute("role", "button");
+        // column.setAttribute("aria-label", `Giorno ${date}`);
 
         date++;
       }
@@ -73,7 +69,9 @@ const createCalendar = () => {
   }
 };
 
-createCalendar(); //DA SISTEMARE
+function dateSelected(date, month, year){
+  alert(`${date}/${month}/${year}`);
+}
 
 const nextMonth = () => {
   thisMonth = thisMonth + 1;
@@ -83,7 +81,7 @@ const nextMonth = () => {
     year = year + 1
     thisMonth = 0
   }
-  createCalendar()
+  buildCalendar()
   return thisMonth
 };
 
@@ -95,6 +93,6 @@ const prevMonth = () => {
     year = year - 1
     thisMonth = 11
   }
-  createCalendar()
+  buildCalendar()
   return thisMonth
 };
