7d39d7022f5b52c731205b3c8fcf9d06af598c3b
7eo03 <leonardosalviato03@gmail.com>
2025-01-08 17:54:41 +0100

    Visualizzazione informazioni header

--- web/map.php
+++ web/map.php
@@ -1,6 +1,9 @@
 <?php
+    session_start();
+    include_once 'header.php';
+
     include_once 'parser.php';
-    $context = ['orario'=>"10:00-17:00"];
+    
 
     $template = new Parser();
     $template->render('map.html',$context);
