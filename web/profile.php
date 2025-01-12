3f505ac4860b41d26652180958692058d9c45f72
7eo03 <leonardosalviato03@gmail.com>
2025-01-12 11:14:20 +0100

    Data ordine con formato corretto

--- web/profile.php
+++ web/profile.php
@@ -17,7 +17,14 @@
 
 
     $context['lastOrder'] = $db->fetchUserLastOrderTickets($_SESSION['username']);
-
+    $splitOrderDateTime = explode(" ", $context['lastOrder'][0]['dataOrarioOrdine']);
+    $orderDate = explode("-", $splitOrderDateTime[0]);
+    $context['orderYear'] = $orderDate[0];
+    $context['orderMonth'] = $orderDate[1];
+    $context['orderDay'] = $orderDate[2];
+    $orderTime = explode(":", $splitOrderDateTime[1]);
+    $context['orderHour'] = $orderTime[0];
+    $context['orderMinute'] = $orderTime[1];
     $db->close();
 
 
