d65eba2ea26a633fad52e87c9dd5b23f3da29dc3
7eo03 <leonardosalviato03@gmail.com>
2025-01-12 17:26:46 +0100

    Adattamento register.html

--- web/register.php
+++ web/register.php
@@ -7,7 +7,7 @@
 
     include_once 'db.php';
 
-    $context['userInfosHeader'] = "";//non mostro i pulsanti login e registrati in alto
+    unset($context['headerBtns']['register']);//non mostro il pulsante registrati in alto
     $errorMessage = "";
     $context['registrationErrorMessage'] = "";
 
