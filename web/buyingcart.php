922c3d60e686143b54de2892df851674aef15689
Vinny <163838894+VinnYuri@users.noreply.github.com>
2025-01-13 20:42:10 +0100

    Update buyingcart.php

--- web/buyingcart.php
+++ web/buyingcart.php
@@ -1,24 +1,42 @@
 <?php
     session_start();
-
-    if(!isset($_SESSION['username'])){
-        header('Location: /login.php'); //da fixare o decidere se dare 401 o 403
+    include_once 'parser.php';
+    
+    if(/*!*/isset($_SESSION['username'])){
+        header('Location: index.php'); //da fixare o decidere se dare 401 o 403
+        exit();   
     }
-
+    
     $debugMsg = '';
     include_once 'db.php';
+    include_once 'header.php';
+
     $db = new DatabaseClient();
     $db->connect();
 
+    $risultato = $db->fetchCart('user');
+    foreach($risultato as $biglietti){
+        if($biglietti['biglietto'] == "1"){
+            $ticket = "Biglieto Ridotto";
+        }
+        else{
+            $ticket = "Biglieto Intero";
+        }
+        $context['cart'] .= "<li>" . $ticket . " x" .  $biglietti['quantita'] . "</li>";
+    }
+
+    /*
     if(isset($_POST['submit'])){
         $db->confirmPayment($_SESSION['username']);
         $debugMsg = 'Pagamento avvenuto con successo!';
-    }
-    
+    }*/
+
+    /* 
     $result = $db->fetchCart($_SESSION['username']);
     $context = ['result'=>$result,'debugMsg'=>$debugMsg];
-
-    include_once 'parser.php';
+    */
+    
+    
     $template = new Parser();
-    $template->render("buyingcart.html",$context);
+    $template->render('buyingcart.html',$context);
 ?>
