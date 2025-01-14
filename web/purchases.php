1c92950f25c719b42f349e08a129525e16806c95
7eo03 <leonardosalviato03@gmail.com>
2025-01-14 17:10:30 +0100

    Pagina visualizzazione acquisti

---
+++ web/purchases.php
@@ -0,0 +1,54 @@
+<?php
+    session_start();
+    include_once 'header.php';
+    unset($context['headerBtns']['profile']);
+    if(!isset($_SESSION['username'])){
+        header('Location: login.php'); //TODO: da rimpiazzare con 401 non autorizzato o 403
+        exit();
+    }
+
+    
+    include_once 'db.php';
+    $db = new DatabaseClient();
+    $db->connect();
+
+    $userDetails = $db->fetchUser($_SESSION['username'])->fetch_assoc();
+    $context['nome'] = $userDetails['nome'];
+
+
+
+
+    $userOrders = $db->fetchOrdersByUser($_SESSION['username']);
+    $context['orders'] = array();
+    foreach($userOrders as $order){
+        $splitOrderDateTime = explode(" ", $order['dataOrarioOrdine']);
+        $orderStdDate = $splitOrderDateTime[0];
+        $orderStdTime = $splitOrderDateTime[1];
+        $orderDate = explode("-", $splitOrderDateTime[0]);
+        $orderYear = $orderDate[0];
+        $orderMonth = $orderDate[1];
+        $orderDay = $orderDate[2];
+        $orderTime = explode(":", $splitOrderDateTime[1]);
+        $orderHour = $orderTime[0];
+        $orderMinute = $orderTime[1];
+        $orderSecond = $orderTime[2];
+        array_push($context['orders'],
+            ['orderId' => $order['id'],
+            'tickets' => $db->fetchTicketsByOrder($order['id']),
+            'orderStdDate' => $orderStdDate,
+            'orderStdTime' => $orderStdTime, 
+            'orderYear' => $orderYear,
+            'orderMonth' => $orderMonth,
+            'orderDay' => $orderDay,
+            'orderHour' => $orderHour,
+            'orderMinute' => $orderMinute,
+            'orderSecond' => $orderSecond]);
+    }
+
+    $context['lastOrder'] = array_shift($context['orders']);
+
+    $db->close();
+    include_once 'parser.php';
+    $template = new Parser();
+    $template->render("purchases.html",$context);
+?>
