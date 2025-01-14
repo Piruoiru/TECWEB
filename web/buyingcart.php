a57ceb8013b2e2400d96af4271f494bb6b1e905a
Vinny <163838894+VinnYuri@users.noreply.github.com>
2025-01-14 13:03:16 +0100

    Update buyingcart.php

--- web/buyingcart.php
+++ web/buyingcart.php
@@ -2,41 +2,38 @@
     session_start();
     include_once 'parser.php';
     
-    if(/*!*/isset($_SESSION['username'])){
+    if(!isset($_SESSION['username'])){
         header('Location: index.php'); //da fixare o decidere se dare 401 o 403
         exit();   
     }
     
-    $debugMsg = '';
     include_once 'db.php';
     include_once 'header.php';
+    $prezzoInt = 34.99;
+    $prezzoRid = 24.99;
 
     $db = new DatabaseClient();
     $db->connect();
 
-    $risultato = $db->fetchCart('user');
+    $risultato = $db->fetchCart($_SESSION['username']);
     foreach($risultato as $biglietti){
         if($biglietti['biglietto'] == "1"){
             $ticket = "Biglieto Ridotto";
+            $context['prezzo'] += $prezzoRid * $biglietti['quantita'];
         }
         else{
             $ticket = "Biglieto Intero";
+            $context['prezzo'] += $prezzoInt * $biglietti['quantita'];
         }
         $context['cart'] .= "<li>" . $ticket . " x" .  $biglietti['quantita'] . "</li>";
     }
 
-    /*
     if(isset($_POST['submit'])){
         $db->confirmPayment($_SESSION['username']);
-        $debugMsg = 'Pagamento avvenuto con successo!';
-    }*/
-
-    /* 
-    $result = $db->fetchCart($_SESSION['username']);
-    $context = ['result'=>$result,'debugMsg'=>$debugMsg];
-    */
-    
-    
+        $context['cart'] = '';
+        $context['prezzo'] = 0.00;
+    }
+  
     $template = new Parser();
-    $template->render('buycart.html',$context);
+    $template->render('buyingcart.html',$context);
 ?>
