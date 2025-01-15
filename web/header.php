b3c7977cd9235ed2e95fc383b8982b7a72cd977e
Vinny <163838894+VinnYuri@users.noreply.github.com>
2025-01-15 14:11:22 +0100

    Update header.php

--- web/header.php
+++ web/header.php
@@ -1,5 +1,7 @@
 <?php
     $context['orario'] = "10:00-17:00";
+    $context['cart'] = '';
+    $context['prezzo'] = 0;
 
     if(isset($_SESSION['username'])){
             $context['headerBtns'] = ['profile' => ['url' => "profile.php", 'text' => 'Il mio profilo'], 'logout'=> ['url' => "logout.php", 'text' => "Logout"]];
