45dbf97aad0e4a430c86976033fb1ca0fca14b18
7eo03 <leonardosalviato03@gmail.com>
2025-01-16 09:58:48 +0100

    Sistemazione breadcrumb

--- web/calendar.php
+++ web/calendar.php
@@ -1,7 +1,9 @@
 <?php
-    include_once 'parser.php';
-    $context = ['orario'=>"10:00-17:00"];
+    session_start();
+    include_once 'header.php';
+    $context['orario'] = "10:00-17:00";
 
+    include_once 'parser.php';
     $template = new Parser();
     $template->render('calendar.html',$context);
 ?>
