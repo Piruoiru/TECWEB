c81dc54064090d68aa3fe8aea9b757d4155d2bf7
7eo03 <leonardosalviato03@gmail.com>
2025-01-23 10:12:36 +0100

    Header refactoring su tutte le pagine per correttezza accessibilità

--- web/header.php
+++ web/header.php
@@ -4,7 +4,7 @@
     $context['prezzo'] = 0;
 
     if(isset($_SESSION['username'])){
-            $context['headerBtns'] = ['profile' => ['url' => "profile.php", 'text' => 'Il mio profilo'], 'logout'=> ['url' => "logout.php", 'text' => "Logout"]];
+            $context['headerBtns'] = ['cart' => ['url' => "buyingcart.php", 'text' => 'Carrello'], 'profile' => ['url' => "profile.php", 'text' => 'Il mio profilo'], 'logout'=> ['url' => "logout.php", 'text' => "Logout"]];
         }else{
             $context['headerBtns'] = ['login' => ['url' => "login.php", 'text' => 'Login'], 'register' => ['url' => "register.php", 'text' => 'Registrati']];
     }
