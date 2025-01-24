3636eed3a9c28c20828bd8094ec7c72d4f18a8da
7eo03 <leonardosalviato03@gmail.com>
2025-01-24 12:19:52 +0100

    Delete profile mobile layout

--- web/deleteProfile.php
+++ web/deleteProfile.php
@@ -3,7 +3,7 @@
     
     session_start();
     if(!isset($_SESSION['username'])){
-        header('Location: index.php');//FIXME: unhautorized?
+        header('Location: index.php');
         exit();
     }
 
